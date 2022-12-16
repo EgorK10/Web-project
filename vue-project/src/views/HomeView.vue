b61a4fb0a77297b43c4f680d7fb01514fd5080bd
EgorK10 <miss.oksana1977@yandex.ru>
2022-12-16 11:31:05 +0800

    Countries done

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container d-flex p-3">
-    <div class="card-group p-3" v-for="team in teams">
+    <div class="card-group p-3" v-for="team in filterTeams">
       <TeamCard :team="team" />
     </div>
   </div>
@@ -11,13 +11,27 @@
 </style>
 
 <script setup>
-import { onBeforeMount, ref } from 'vue';
+import {onBeforeMount, computed, ref, watch} from "vue"
+import {useRoute} from "vue-router"
 import axios from "axios";
 import TeamCard from '@/components/TeamCard.vue';
 const teams = ref([]);
+const route = useRoute();
+const country = ref(route.query.countryId);
 
 onBeforeMount(async () => {
   let request = (await axios.get("/api/teams")).data
   teams.value = request.items
 })
+
+watch(route, async () => {
+  country.value = route.query.countryId
+})
+
+const filterTeams = computed(() => {
+  return teams.value.filter(team =>
+    country.value === undefined || team.country_id.toString() === country.value
+  )
+})
+
 </script>
